388d86d36b386395fa16d3c5af8ecbdd40dbc77e
chilejay7 <codyburk7@yahoo.com>
2023-12-21 21:12:17 -0700

    comments added to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,11 @@
+// The connect and connection properties are destructured from the Mongoose module.
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/socialMedia')
+// This establishes a connection with the notFacebook database.
+// 27017 is the default port for MongoDB.
+connect('mongodb://127.0.0.1:27017/notFacebook')
 .then(() => {
-	console.log(`Database connection open`)
+	console.log(`Database connection open.`)
 })
 .catch (err => {
 	console.log(`The database failed to connect.  The error is: ${err}`)
