5bb2bbd050cff5c967339c32c950f335fad43e3d
chilejay7 <codyburk7@yahoo.com>
2023-12-21 21:37:20 -0700

    additional comments added before creating new branch

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+// This GET route will respond to requests sent to the /api root route.
 router.get('/', (req, res) => {
     console.log(`API routes are working`);
 })
