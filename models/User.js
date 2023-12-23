8a318a1bdb20e4dff436a2b58756297a07ae4ad1
chilejay7 <codyburk7@yahoo.com>
2023-12-23 11:20:25 -0700

    comments added to User model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,6 @@
+// Imports the schema and model properties from mongoose.
+// Schema is used to define the structure of our documents in MongoDB.
+// Model is a function within Mongoose that allows us to create a model based on the Schema. Models represent the documents in our MongoDB collection.
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
@@ -26,9 +29,13 @@ const userSchema = new Schema({
     ],
 })
 
-userSchema.virtual('friendCount').get(() => {
+// This creates a virtual property called friendCount on the userSchema.
+// It uses a getter function that retrieves the length of the user's friends array, providing a count for the number of friends a user has.
+// Calling user.friendCount on a specific document will calculate the number of friends a user has.
+userSchema.virtual('friendCount').get(function () {
     return this.friends.length;
 });
+
 
 const User = model('user', userSchema);
 
