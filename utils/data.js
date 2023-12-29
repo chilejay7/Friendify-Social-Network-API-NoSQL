dc4d5098237f2cc458d3454f925a3188036486d6
chilejay7 <codyburk7@yahoo.com>
2023-12-29 14:25:02 -0700

    thoughts and reaction statements created for seeds

--- utils/data.js
+++ utils/data.js
@@ -1,3 +1,4 @@
+// This creates an array of usernames to be used with the models and schema.
 const usernames = [
 'code',
 'naranja',
@@ -10,8 +11,29 @@ const usernames = [
 'jaybaby',
 'papaJ',
 'kat',
+'lela',
 ];
 
+// This array can be used to generate random thoughts.
+const thoughtStatements = [
+    `I'm not sure I like this.`,
+    `Who are you?`,
+    `This is neat!`,
+    `I can't believe I didn't think of this.`,
+    `Thanks for sharing.`,
+    `What is this?`,
+    `what time does the game start tonight?`,
+    `It seems far away.`,
+    `Are you sure this is right?`,
+];
+
+// This is used to generate random array items for the models and seed data.
+const getRandomItem = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+};
+
+// The users variable defines an empty array that will hold the user objects to be inserted into the User model.
+// This loop creates the usernames and email addresses needed for each user.  The array is exported to the seeds.js file.
 const users = [];
 
 for (let i = 0; i < usernames.length; i++) {
@@ -21,4 +43,31 @@ for (let i = 0; i < usernames.length; i++) {
     })
 };
 
-module.exports = { users };
+// This function is used to generate random reactions using the thoughtStatements array for reaction statements.
+// The function is referenced in the createThoughts function to create the seed data.
+const generateReactions = (num) => {
+    const reactionData = [];
+    for (let i = 0; i < num; i++) {
+        reactionData.push({
+            reactionBody: getRandomItem(thoughtStatements),
+            username: getRandomItem(usernames),
+        })
+    }
+    return reactionData;
+}
+
+// This function creates the data needed to seed the thoughts collection.  A number is accepted as an argument in the function and will generate the specified number
+// of thoughts as objects with the required fields.
+const createThoughts = (num) => {
+    let thoughtData = [];
+    for (let i = 0; i < num; i++) {
+        thoughtData.push({
+            thoughtText: getRandomItem(thoughtStatements),
+            username: getRandomItem(usernames),
+            reactions: [generateReactions(2)],
+        })
+    }
+    return thoughtData;
+}
+
+module.exports = { users, getRandomItem, generateReactions };
--- utils/seeds.js
+++ utils/seeds.js
@@ -1,12 +1,13 @@
 const connection = require('../config/connection');
 const { Reaction, Thought, User } = require('../models');
-const { users } = require('./data');
+const { users, getRandomItem } = require('./data');
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
     console.log('The database connection is open');
 
+    // The code that follows will check to see if the colletions specified already exist.  If they do, they will be dropped.  There is a check for both user and thoughts.
     let userCheck = await connection.db.listCollections({ name: 'user' }).toArray();
     userCheck.length ? await connection.dropCollection('user') : console.log('user collection does not exist');
 
@@ -14,6 +15,8 @@ connection.once('open', async () => {
     thoughtCheck.length ? await connection.dropCollection('thoughts') : console.log('thoughts collection does not exist');
 
     await User.collection.insertMany(users);
+
+    await Thought.collection.insertMany()
 
     console.table(users);
 
