a2111100e7b5923d3b2552df3c7e855536de21eb
chilejay7 <codyburk7@yahoo.com>
2023-12-30 12:25:16 -0700

    comments added to routes files

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const { getThoughts, getOneThought } = require('../../controllers/thoughtController');
 
+// This route uses a find query to retrieve all thoughts in the database.
 router.route('/').get(getThoughts);
+
+// This route uses the _id of a specific document to retrieve one specific thought from the database.
 router.route('/:id').get(getOneThought);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-
 const { getUsers, getOneUser } = require('../../controllers/userController');
 
+// This route uses a find query to retrieve all users in the database.
 router.route('/').get(getUsers);
+
+// This route uses the _id of a specific document to retrieve one specific user from the database.
 router.route('/:id').get(getOneUser);
 
 module.exports = router;
