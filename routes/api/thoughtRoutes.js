586d976c50304ff17afcebc1df4aebb3a8f02e67
chilejay7 <codyburk7@yahoo.com>
2024-01-01 11:55:37 -0700

    reactions route added

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getThoughts, getOneThought, createThought, updateThought, deleteThought } = require('../../controllers/thoughtController');
+const { getThoughts, getOneThought, createThought, updateThought, deleteThought, addThoughtResponse } = require('../../controllers/thoughtController');
 
 // This route uses a find query to retrieve all thoughts in the database.
 router.route('/')
@@ -11,5 +11,8 @@ router.route('/:id')
 .get(getOneThought)
 .put(updateThought)
 .delete(deleteThought);
+
+// This is a separate route for creating reactions on thoughts.
+router.route('/:id/reactions').post(addThoughtResponse);
 
 module.exports = router;
