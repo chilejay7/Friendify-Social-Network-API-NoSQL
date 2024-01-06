9313e24586a7ac549a5d3670d8c5e71885b1f1ed
chilejay7 <codyburk7@yahoo.com>
2024-01-05 21:29:04 -0700

    comments added to the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,6 +23,7 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    // This route assumes the request's body will follow the model and include the required fields of a username and email address.
     async createUser(req, res) {
         try {
             const newUser = await User.create(req.body);
@@ -32,6 +33,9 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    // The _id of the user to be updated will be included in the request's parameters.  The body of the request will contain the fields that need to be updated.
+    // The runValidators and new statements run the validations set on the models and return the newly modified object.
+    // The default is to return the old object even though it has been modified.  To see updated values we need to use the new: true option.
     async updateUser(req, res) {
         try {
             const { id } = req.params;
@@ -51,6 +55,7 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    // The id of the specific user document is destructured from the request's parameters included in the URL.
     async deleteUser(req, res) {
         try {
             const { id } = req.params;
@@ -66,6 +71,7 @@ module.exports = {
             res.status(500).json(err)
         }
     },
+    // The request's body should include the _id of another user in the database to add to the friends array.  The request will be sent in JSON format.
     async addFriend(req, res) {
         try {
             const { id } = req.params;
@@ -81,6 +87,8 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+     // The id of the user and the friend are both destructured from the request's parameters included in the URL.
+    // The pull operator is used to remove an object from the nested array using the friendId parameter.
     async removeFriend(req, res) {
         try {
             const { userId, friendId } = req.params;
